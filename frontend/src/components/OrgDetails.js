748db8a69720dc21a8d4db7c88a9f7e4101b4a14
andre_sfnd <andre_sfnd@usp.br>
2019-08-24 20:01:14 -0300

    Botao enviar email

--- frontend/src/components/OrgDetails.js
+++ frontend/src/components/OrgDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Tabs, Typography, Progress } from 'antd';
+import { Card, Tabs, Typography, Progress, Button } from 'antd';
 import './OrgDetails.css';
 
 const { TabPane } = Tabs;
@@ -15,7 +15,12 @@ class OrgDetails extends React.Component {
                     cover={<img alt="example" style={{maxWidth:250, maxHeight: 350, float:'left', padding: '50px 20px'}} src="https://bixo.saecomp.com.br/img/extra/e3.png" />}
                 >
                     <div style={{marginTop:'26px', width:'75%', height:250, display: 'inline-block', textAlign:'left'}}>
-                        <Title level={2}>Bandejovis</Title>
+                        <div style={{display:'inline-block'}}>
+                            <Title level={2} style={{float:'left'}}>Bandejovis</Title>
+                            <Button href="mailto:mail@example.com" type="primary" shape="round" icon="mail" style={{float:'right', marginTop: 5, position:'absolute', right:30, height:40, paddingTop:3}}>
+                                Enviar email
+                            </Button>
+                        </div>
                         <Paragraph ellipsis={{ rows: 9, expandable: true }}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac ultrices nisi. In nec blandit nibh, ut malesuada est. Nulla facilisi. Praesent ut justo suscipit, consectetur lorem auctor, blandit nisl. Donec dapibus dolor quis massa maximus fermentum. Nunc pharetra orci velit, sit amet sodales mi accumsan non. Fusce tempor, sapien eget convallis auctor, tortor nunc tincidunt ligula, ac rutrum ante mi vitae lectus. Nam nec euismod est. Maecenas accumsan id dui non dignissim. Etiam eu nulla ac lectus lacinia ultricies a nec turpis. Maecenas ac diam felis.Etiam dictum in felis vitae porttitor. Nulla nec imperdiet erat. Fusce porttitor odio vel euismod tincidunt. Cras tincidunt efficitur ex venenatis bibendum. Fusce sed purus enim. In sit amet laoreet massa. Ut ultricies euismod purus eu tincidunt. Pellentesque hendrerit hendrerit volutpat. Pellentesque ac suscipit risus, nec pellentesque leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
                         </Paragraph>
