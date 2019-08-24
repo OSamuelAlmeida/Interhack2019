c8a244d7a526424f1ca49b436e81a4dcd2b44270
Samuel Almeida <samuelalmeida13@gmail.com>
2019-08-24 19:04:47 -0300

    Adicionadas rotas para Login e Registro

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import OrgPage from './components/OrgPage';
 import OrgDetails from './components/OrgDetails';
 import AddNewOrg from './components/AddNewOrg';
+import Login from './components/Login';
+import Register from './components/Register';
 const { Header, Content, Footer } = Layout;
 
 function AppRouter() {
@@ -19,12 +21,16 @@ function AppRouter() {
             >
               <Menu.Item key="1"><Link to="/">Inicio</Link></Menu.Item>
               <Menu.Item key="2"><Link to="/details/1">Detalhes</Link></Menu.Item>
+              <Menu.Item key="3" style={{float: 'right'}}><Link to="/login">Entrar</Link></Menu.Item>
+              <Menu.Item key="4" style={{float: 'right'}}><Link to="/register">Registrar</Link></Menu.Item>
             </Menu>
           </Header>
           <Content style={{ padding: '50px', backgroundColor: '#F0F0F0' }}>
             <Route path="/" exact component={OrgPage} />
             <Route path="/details/:id" exact component={OrgDetails} />
             <Route path="/add-new-org" exact component={AddNewOrg} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
           </Content>
       </Router>
     );
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Register extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Página de Registro</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
