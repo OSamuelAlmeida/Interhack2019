790de77934dd3448665309363850fa59912d1359
Samuel Almeida <samuelalmeida13@gmail.com>
2019-08-24 18:52:24 -0300

    Adicionado botão de voltar

---
+++ frontend/src/components/BackButton.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { Button, Icon } from 'antd';
+import React from 'react';
+
+class BackButton extends React.Component {
+    render() {
+        return (
+            <div style={{textAlign: 'left', margin: '10px'}}>
+                <Link to={this.props.to}>
+                    <Button type="primary">
+                        <Icon type="left" />
+                            Voltar
+                    </Button>
+                </Link>
+            </div>
+        );
+    } 
+}
+
+export default BackButton;
