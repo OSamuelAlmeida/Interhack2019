5df4169d027aaa30663e1bb4a03009e6674803fc
Samuel Almeida <samuelalmeida13@gmail.com>
2019-08-24 17:34:34 -0300

    Adicionado botão de adição de novos grupos

---
+++ frontend/src/components/AddOrgCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Progress } from 'antd';
+
+class AddOrgCard extends React.Component {
+    render() {
+        return (
+            <Link to={"add-new-org/" + this.props.id}>
+                <Card style={{width: 300 }}>
+                    <div className="custom-image">
+                        <img alt="example" width="100%" height="200px" src={process.env.PUBLIC_URL + '/img/add.png'} />
+                    </div>
+                    <div className="custom-card">
+                        <h3>Adicionar</h3>
+                    </div>
+                </Card> 
+            </Link>
+
+        );
+    }
+}
+
+export default AddOrgCard;
--- frontend/src/components/OrgList.js
+++ frontend/src/components/OrgList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Col, Row } from 'antd';
 import OrgCard from './OrgCard';
+import AddOrgCard from './AddOrgCard';
 
 class OrgList extends React.Component {
     render() {
         return (
-            <div style={{margin: '50px'}}>
-                <Row gutter={16}>
+            <div>
+                <Row style={{margin: '50px'}} gutter={16}>
                     <Col span={8}>
                         <OrgCard id={1} title="CAASO" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
                    </Col>
@@ -16,6 +17,11 @@ class OrgList extends React.Component {
                     <Col span={8}>
                         <OrgCard id={3} title="Teste 3" percent={100}/>
                    </Col>
+                </Row>
+                <Row style={{margin: '50px'}} gutter={16}>
+                    <Col span={8}>
+                        <AddOrgCard/>
+                    </Col>
                 </Row>
             </div>
         );
