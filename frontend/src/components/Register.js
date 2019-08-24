5e3ddc8994df09bd834838ccb72395d4c6330229
Samuel Almeida <samuelalmeida13@gmail.com>
2019-08-24 20:31:37 -0300

    Adicionado tela de registros

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { 
+    Form, 
+    Button, 
+    Upload, 
+    Icon,
+    Input,
+    message
+} from 'antd';
+import BackButton from './BackButton';
+
+import './css/CustomForm.css';
+
+const formItemLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 10 },
+};
 
 class Register extends React.Component {
+    handleSubmit = (e) => {
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>Página de Registro</h1>
-            </div>
+            <React.Fragment>
+                <BackButton to="/" />
+                <div className="form-div">
+                    <h1>Registrar</h1>
+                    <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                        <Form.Item label="E-mail">
+                            <Input /> 
+                        </Form.Item> 
+                        <Form.Item label="Senha">
+                            <Input.Password name="password"/> 
+                        </Form.Item> 
+                        <Form.Item label="Confirmar senha">
+                            <Input.Password name="confirm-password"/> 
+                        </Form.Item> 
+
+                        <Form.Item wrapperCol={{
+                            sm: { span: 16, offset: 5 },
+                        }} >
+                            <Button style={{ width: '200px', height: '50px'}} type="primary" htmlType="submit" className="login-form-button">
+                                Entrar
+                            </Button> 
+                        </Form.Item>
+                    </Form>
+                </div>
+            </React.Fragment>
         );
     }
 }
