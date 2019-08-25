be60d7740fad6651dd5bc50ebbcb83e3a4340c33
andre_sfnd <andre_sfnd@usp.br>
2019-08-25 02:58:12 -0300

    conflito dnv

--- frontend/src/components/OrgList.js
+++ frontend/src/components/OrgList.js
@@ -105,51 +105,51 @@ class OrgList extends React.Component {
             const topThree = (
             <div style={{width:'90%', margin:'0 auto'}}>
 
-            <Row>
-                <Col span={16}>
-                    <div style={{width:'90%', margin:'0 auto'}}>
-                        <Title level={2} style={{float:'left'}}>Organizações em Destaque</Title>
-                    </div>
-                </Col>
-                <Col span={8}></Col>
-            </Row>
+                <Row>
+                    <Col span={16}>
+                        <div style={{width:'90%', margin:'0 auto'}}>
+                            <Title level={2} style={{float:'left'}}>Organizações em Destaque</Title>
+                        </div>
+                    </Col>
+                    <Col span={8}></Col>
+                </Row>
 
-            <Row style={{marginTop:'20px'}}>
-                <Col span={24}>
-                    <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
-                </Col>
-            </Row>
+                <Row style={{marginTop:'20px'}}>
+                    <Col span={24}>
+                        <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
+                    </Col>
+                </Row>
 
-            <Row style={{marginTop:'20px'}}>
-                <Col span={24}>
-                    <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
-                </Col>
-            </Row>
+                <Row style={{marginTop:'20px'}}>
+                    <Col span={24}>
+                        <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
+                    </Col>
+                </Row>
 
-            <Row style={{marginTop:'20px'}}>
-                <Col span={24}>
-                    <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
-                </Col>
-            </Row>
+                <Row style={{marginTop:'20px'}}>
+                    <Col span={24}>
+                        <ExtendedCard id={1} title="CAASO" description="blablabla" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
+                    </Col>
+                </Row>
 
-            <Row style={{marginTop:'70px'}}>
-                <Col span={16}>
-                    <div style={{width:'90%', margin:'0 auto'}}>
-                        <Title level={2} style={{float:'left'}}>Organizações do Campus</Title>
-                    </div>
-                </Col>
-                <Col span={8}>
-                    <div style={{width:'300px', margin:'0 auto', textAlign:'right'}}>
-                        <Input.Search
-                            placeholder="Pesquisar..."
-                            style={{ width: 300, height:40 }}
-                            onSearch={value => console.log(value)}
-                        />
-                    </div>
-                </Col>
-            </Row>
+                <Row style={{marginTop:'70px'}}>
+                    <Col span={16}>
+                        <div style={{width:'90%', margin:'0 auto'}}>
+                            <Title level={2} style={{float:'left'}}>Organizações do Campus</Title>
+                        </div>
+                    </Col>
+                    <Col span={8}>
+                        <div style={{width:'300px', margin:'0 auto', textAlign:'right'}}>
+                            <Input.Search
+                                placeholder="Pesquisar..."
+                                style={{ width: 300, height:40 }}
+                                onSearch={value => console.log(value)}
+                            />
+                        </div>
+                    </Col>
+                </Row>
 
-        </div>);
+            </div>);
 
 
             return [ topThree, getRows() ];
