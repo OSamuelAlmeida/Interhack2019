97bdc48a7a679246b4aa2d9406b8b866a7a5fa63
Samuel Almeida <samuelalmeida13@gmail.com>
2019-08-25 02:07:05 -0300

    Carregando lista de organizações do banco

--- frontend/src/components/OrgList.js
+++ frontend/src/components/OrgList.js
@@ -1,51 +1,117 @@
 import React from 'react';
-import { Col, Row, Input } from 'antd';
+import axios from 'axios';
+import { Col, Row, Input, Spin } from 'antd';
 import OrgCard from './OrgCard';
 import AddOrgCard from './AddOrgCard';
 
 class OrgList extends React.Component {
-    render() {
-        return (
-            <div style={{width:'80%', margin:'0 10%'}}>
-
-                <Row>
-                    <Col span={8}><div style={{width:'300px'}}></div></Col>
-                    <Col span={8}><div style={{width:'300px'}}></div></Col>
-                    <Col span={8}>
-                        <div style={{width:'300px', margin:'0 auto', textAlign:'right'}}>
-                            <Input.Search
-                                placeholder="Pesquisar..."
-                                style={{ width: 200 }}
-                                onSearch={value => console.log(value)}
-                            />
-                        </div>
-                    </Col>
-                </Row>
-                <Row style={{marginTop:'50px'}}>
-                    <Col span={8}>
-                        <OrgCard id={1} title="CAASO" percent={10} image="https://minervacaaso.files.wordpress.com/2017/02/logo_caaso_colegio.png?w=425&h=329"/>
-                    </Col>
-                    <Col span={8}>
-                        <OrgCard id={2} title="Teste 2" percent={50}/>
-                    </Col>
-                    <Col span={8}>
-                        <OrgCard id={3} title="Teste 3" percent={100}/>
-                    </Col>
-                </Row>
-                <Row style={{marginTop:'50px'}}>
-                    <Col span={8}>
-                        <AddOrgCard/>
-                    </Col>
-                    <Col span={8}>
-                        
-                    </Col>
-                    <Col span={8}>
-                        
-                    </Col>
-                </Row>
-            </div>
-        );
+    state = {
+        isLoading: true, 
+        groups: null,
+        groupsPercent: []
     }
+
+    componentDidMount() {
+        var groups = null;
+        var groupsPercent = []
+
+        axios.all([
+            axios.get('http://172.26.135.171:8080/api/group/'),
+            axios.get('http://172.26.135.171:8080/api/meta/')
+        ]).then(axios.spread((groupsData, metasData) => {
+            groups = groupsData.data;
+
+            const all_metas = metasData.data;
+
+            for (var i = 0; i < groups.length; ++i) {
+                console.log(i);
+                var group_id = groups[i].id;
+                console.log(group_id);
+                var metasCount = 0;
+                var metasCompleted = 0;
+    
+                for (var j = 0; j < all_metas.length; ++j) {
+                    if (all_metas[j].group == group_id) {
+                        metasCount++;
+    
+                        if (all_metas[j].status == 3) {
+                            metasCompleted++;
+                        }
+                    }
+                }
+
+                var metasPercent = 0;
+    
+                if (metasCount > 0) {
+                    metasPercent = metasCompleted / metasCount;
+                }
+
+                groupsPercent.push(metasPercent);
+            }
+
+            console.log('O CARAIO 111!!!!!!');
+            console.log(groups);
+            console.log(groupsPercent);
+
+            this.setState({
+                ...this.state,
+                isLoading: false,
+                groups: groups,
+                groupsPercent: groupsPercent
+            });
+        }));
+    }
+
+    render() {
+        const getPage = () => {
+
+            const getRows = () => {
+                var columns = [];
+                var currentIndex = 0;
+                var rowsCount = 1
+
+                while (currentIndex < this.state.groups.length) {
+                    if (currentIndex % 3 == 0) {
+                        rowsCount++;
+                    }
+
+                    var column = (
+                        <Col span={8}>
+                            <OrgCard id={currentIndex} title={this.state.groups[currentIndex].name} percent={this.state.groupsPercent[currentIndex]} image={this.state.groups[currentIndex].logo_url}/>
+                        </Col>
+                    );
+                    
+                    columns.push(column);
+                    currentIndex++;
+                }
+
+                var rows = [];
+                currentIndex = 0;
+
+                for (var i = 0; i < rowsCount; ++i) {
+                    var currentColumns = [];
+
+                    for (var i = 0; i < 3; ++i) {
+                        currentColumns.push(columns[currentIndex]);
+                        currentIndex++;
+                    }
+
+                    rows.push(
+                        <Row style={{marginTop:'50px'}}>
+                            {currentColumns}
+                        </Row>
+                    );
+                }
+
+                return rows;
+            };
+
+            return getRows();
+        };
+
+
+        return this.state.isLoading ? <Spin /> : getPage();
+    };
 }
 
 export default OrgList;
